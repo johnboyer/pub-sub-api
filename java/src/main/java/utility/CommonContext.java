c4fcc03f2b4a38a6dc8fe63fe969d553c2a46cb0
John Boyer <john.boyer@accolade.com>
2024-08-08 10:20:49 -0700

    [Checkin] Java quick start changes

--- java/src/main/java/utility/CommonContext.java
+++ java/src/main/java/utility/CommonContext.java
@@ -190,7 +190,7 @@ public class CommonContext implements AutoCloseable {
     public GenericRecord createEventMessage(Schema schema) {
         // Update CreatedById with the appropriate User Id from your org.
         return new GenericRecordBuilder(schema).set("CreatedDate", System.currentTimeMillis())
-                .set("CreatedById", "<User_Id>").set("Order_Number__c", "1")
+                .set("CreatedById", "4nyvqrd5bp@privaterelay.appleid.com").set("Order_Number__c", "1")
                 .set("City__c", "Los Angeles").set("Amount__c", 35.0).build();
     }
 
@@ -207,7 +207,7 @@ public class CommonContext implements AutoCloseable {
     public GenericRecord createEventMessage(Schema schema, final int counter) {
         // Update CreatedById with the appropriate User Id from your org.
         return new GenericRecordBuilder(schema).set("CreatedDate", System.currentTimeMillis())
-                .set("CreatedById", "<User_Id>").set("Order_Number__c", String.valueOf(counter+1))
+                .set("CreatedById", "4nyvqrd5bp@privaterelay.appleid.com").set("Order_Number__c", String.valueOf(counter+1))
                 .set("City__c", "Los Angeles").set("Amount__c", 35.0).build();
     }
 
@@ -221,7 +221,7 @@ public class CommonContext implements AutoCloseable {
         List<GenericRecord> events = new ArrayList<>();
         for (int i=0; i<numEvents; i++) {
             events.add(new GenericRecordBuilder(schema).set("CreatedDate", System.currentTimeMillis())
-                    .set("CreatedById", "<User_Id>").set("Order_Number__c", orderNumbers[i % 5])
+                    .set("CreatedById", "4nyvqrd5bp@privaterelay.appleid.com").set("Order_Number__c", orderNumbers[i % 5])
                     .set("City__c", cities[i % 5]).set("Amount__c", amounts[i % 5]).build());
         }
 
